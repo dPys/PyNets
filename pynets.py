Fix arg parsing, add ICA masking option

pynets.py
@@ -17,48 +17,53 @@
 #    questions about licensing and copyright/patent restrictions with openDTI, please
 #    contact the primary author, Derek Pisner, at dpisner@utexas.edu
 import sys
+import argparse
 from nipype.interfaces.base import isdefined,Undefined
 from sklearn.model_selection import train_test_split
 ######################
-return_list = "\n\n\nPyNets help: \n\n\nYou must include: \n1) Either a path to a functional image in standard space and .nii or .nii.gz format OR an input time-series text/csv file, in quotes \n2) A subject ID (numerical only) for those files in quotes\n\n\n*If a functional image file is used, you must also select: \n3) An atlas from the list below in quotes \n4) A TR value in quotes\n5) *Optionally, an atlas-defined (Power et al. 2011) RSN name, in quotes, from the list below from which to extract local time-series graphs \n6) *Optionally, a threshold, in quotes, indicating a proportion of weights to preserve in the graph (default is 90%) \n7) *Optionally, a node radius size, in quotes (default is 5 voxels) \n\n\nATLASES:\n'abide_pcp'\n'adhd'\n'atlas_aal'\n'atlas_basc_multiscale_2015'\n'atlas_craddock_2012'\n'atlas_destrieux_2009'\n'atlas_harvard_oxford'\n'atlas_msdl'\n'atlas_smith_2009'\n'atlas_yeo_2011'\n'cobre'\n'coords_dosenbach_2010'\n'coords_power_2011'\n'haxby'\n'haxby_simple'\n'icbm152_2009'\n'icbm152_brain_gm_mask'\n'megatrawls_netmats'\n'mixed_gambles'\n'miyawaki2008'\n'nyu_rest'\n'oasis_vbm'\n\n\nRSN's:\n'DMN'\n'FPTC'\n'DA'\n'SN'\n'VA'"
+return_list = "\n\n\nPyNets help: \n\n\nYou must include: \n1) Either a path to a functional image in standard space and .nii or .nii.gz format OR an input time-series text/csv file, using the -i flag \n2) A subject ID (numerical only) for those files, using the -ID flag \n3) An atlas from the list below, using the -a flag \n\n\n*If a functional image file is used, you must also select: \n4) *Optionally, a binarized mask image (e.g. an ICA network volume) to use only those network seeds contained within the mask\n5) *Optionally, an atlas-defined (Power et al. 2011) network name, using the -n flag, from the list below from which to extract local time-series graphs \n6) *Optionally, a threshold, using the -thr flag, indicating a proportion of weights to preserve in the graph (default is 90%) \n7) *Optionally, a node radius size, using the -ns flag (default is 3 voxels) \n\n\nATLASES:\n'abide_pcp'\n'adhd'\n'atlas_aal'\n'atlas_basc_multiscale_2015'\n'atlas_craddock_2012'\n'atlas_destrieux_2009'\n'atlas_harvard_oxford'\n'atlas_msdl'\n'atlas_smith_2009'\n'atlas_yeo_2011'\n'cobre'\n'coords_dosenbach_2010'\n'coords_power_2011'\n'haxby'\n'haxby_simple'\n'icbm152_2009'\n'icbm152_brain_gm_mask'\n'megatrawls_netmats'\n'mixed_gambles'\n'miyawaki2008'\n'nyu_rest'\n'oasis_vbm'\n\n\nRSN's:\n'DMN'\n'FPTC'\n'DA'\n'SN'\n'VA'"
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i')
+    parser.add_argument('-ID')
+    parser.add_argument('-a')
+    parser.add_argument('-n')
+    parser.add_argument('-thr')
+    parser.add_argument('-ns')
+    parser.add_argument('-m')
+    args = parser.parse_args()
+
 if '-h' in sys.argv:
     print(return_list)
     sys.exit()
 elif len(sys.argv) > 1:
-    try:
-        input_file=sys.argv[1]
-    except:
-        print("Error: You must include a file path, in quotes, to either a standard space functional image in .nii or .nii.gz format or a path to a time-series text/csv file")
+    input_file=args.i
+    if input_file is None:
+        print("Error: You must include a file path to either a standard space functional image in .nii or .nii.gz format or a path to a time-series text/csv file, with the -i flag")
         sys.exit()
-    try:
-        ID=sys.argv[2]
-    except:
-        print("Error: You must include a subject ID, in quotes, as your second entry on the command line")
+    ID=args.ID
+    if ID is None:
+        print("Error: You must include a subject ID in your command line call")
         sys.exit()
     if '.nii' in input_file:
-        try:
-            atlas_select=sys.argv[3]
-        except:
-            print("Error: You have specified the path, in quotes, to an image file. You must also include an atlas name as the third argument to your PyNets.py call")
-            sys.exit()
-    else:
-        atlas_select=''
+        atlas_select=args.a
+        if atlas_select is None:
+            atlas_select='coords_power_2011'
+            print("Using Power 264-node atlas (default)...")
 else:
     print("\nMissing command-line inputs!\n" + return_list)
     sys.exit()
-try:
-    NETWORK=sys.argv[4]
-except:
-    NETWORK=''
-try:
-    thr=sys.argv[5]
-except:
-    thr='0.9'
-try:
-    node_size=sys.argv[6]
-except:
+NETWORK=args.n
+if NETWORK is None:
+    print("Extracting global net stats...")
+thr=args.thr
+if thr is None:
+    thr='0.95'
+node_size=args.ns
+if node_size is None:
     node_size='3'
-
+mask=args.m
 ######################
 
 import warnings
@@ -89,24 +94,23 @@ import_list=["import nilearn", "import numpy as np", "import os", "import bct", 
 print("\n\n\n")
 print ("INPUT FILE: " + input_file)
 print("\n")
-print ("SUBJECT ID: " + ID)
+print ("SUBJECT ID: " + str(ID))
 if '.nii' in input_file:
     print("\n")
-    print ("ATLAS: " + atlas_select)
+    print ("ATLAS: " + str(atlas_select))
     print("\n")
-    if NETWORK != '':
-        print ("NETWORK: " + NETWORK)
+    if NETWORK != None:
+        print ("NETWORK: " + str(NETWORK))
 print("\n\n\n")
 dir_path = os.path.dirname(os.path.realpath(input_file))
 
 pynets_dir = os.path.dirname(os.path.abspath(__file__))
-
 #print(pynets_dir)
 #sys.exit()
 
 ##Import/generate time-series and estimate GLOBAL covariance/sparse inverse covariance matrices
-def import_mat_func(input_file, ID, atlas_select, NETWORK, pynets_dir, node_size):
-    if '.nii' in input_file and NETWORK == '':
+def import_mat_func(input_file, ID, atlas_select, NETWORK, pynets_dir, node_size, mask):
+    if '.nii' in input_file and NETWORK == None:
         func_file=input_file
         dir_path = os.path.dirname(os.path.realpath(func_file))
         atlas = getattr(datasets, 'fetch_%s' % atlas_select)()
@@ -118,8 +122,22 @@ def import_mat_func(input_file, ID, atlas_select, NETWORK, pynets_dir, node_size
         print("\n")
         print('Stacked atlas coordinates in array of shape {0}.'.format(coords.shape))
         print("\n")
-        spheres_masker = input_data.NiftiSpheresMasker(
-            seeds=coords, radius=float(node_size), memory='nilearn_cache')
+        if mask is None:
+            spheres_masker = input_data.NiftiSpheresMasker(
+                seeds=coords, radius=float(node_size), memory='nilearn_cache', memory_level=5, verbose=2, standardize=True)
+        else:
+            from nilearn import masking
+            from scipy import stats
+            mask_data, _ = masking._load_mask_img(mask)
+            mask_coords = list(zip(*np.where(mask_data != 0)))
+            for coord in coords:
+                if tuple(coord) in mask_coords:
+                    print('Removing coordinate: ' + str(tuple(coord)) + ' since it falls outside of network mask...')
+                    ix = stats.mode(np.where(coords == coord)[0])[0][0]
+                    coords = np.delete(coords, ix, axis=0)
+                    print(str(len(coords)))
+            spheres_masker = input_data.NiftiSpheresMasker(
+                seeds=coords, radius=float(node_size), memory='nilearn_cache', memory_level=5, verbose=2, standardize=True)
         time_series = spheres_masker.fit_transform(func_file)
         correlation_measure = ConnectivityMeasure(kind='correlation')
         correlation_matrix = correlation_measure.fit_transform([time_series])[0]
@@ -142,35 +160,44 @@ def import_mat_func(input_file, ID, atlas_select, NETWORK, pynets_dir, node_size
         np.savetxt(time_series_path, time_series, delimiter='\t')
         mx = genfromtxt(time_series_path, delimiter='')
 
-    elif '.nii' in input_file and NETWORK != '':
+    elif '.nii' in input_file and NETWORK != None:
         func_file=input_file
 
         ##Reference RSN list
-	load_path= pynets_dir + '/RSN_refs/' + NETWORK + '_coords.csv'
-	df = pd.read_csv(load_path).ix[:,0:4]
-	i=1
-	coords = []
-	labels = []
-	for i in range(len(df)):
-  	    print("ROI Reference #: " + str(i))
-  	    x = int(df.ix[i,1])
-  	    y = int(df.ix[i,2])
-  	    z = int(df.ix[i,3])
-  	    print("X:" + str(x) + " Y:" + str(y) + " Z:" + str(z))
-  	    coords.append((x, y, z))
-  	    labels.append(i)
-  	print("\n")
- 	print(coords)
-  	print(labels)
-  	print("\n")
-  	print("-------------------")
-  	i + 1	
+    	load_path= pynets_dir + '/RSN_refs/' + NETWORK + '_coords.csv'
+    	df = pd.read_csv(load_path).ix[:,0:4]
+    	i=1
+    	coords = []
+    	labels = []
+    	for i in range(len(df)):
+      	    print("ROI Reference #: " + str(i))
+      	    x = int(df.ix[i,1])
+      	    y = int(df.ix[i,2])
+      	    z = int(df.ix[i,3])
+      	    print("X:" + str(x) + " Y:" + str(y) + " Z:" + str(z))
+      	    coords.append((x, y, z))
+      	    labels.append(i)
+      	print("\n")
+     	print(coords)
+      	print(labels)
+      	print("\n")
+      	print("-------------------")
+      	i + 1
         dir_path = os.path.dirname(os.path.realpath(func_file))
 
         ##Grow ROIs
+        ##If masking, remove those coords that fall outside of the mask
+        if mask != None:
+            from nilearn import masking
+            mask_data, _ = masking._load_mask_img(mask)
+            mask_coords = list(zip(*np.where(mask_data != 0)))
+            for coord in coords:
+                if coord in mask_coords:
+                    print('Removing coordinate: ' + str(coord) + ' since it falls outside of network mask...')
+                    coords.remove(coord)
         masker = input_data.NiftiSpheresMasker(
-            coords, radius=float(node_size), allow_overlap=True,
-            memory='nilearn_cache', verbose=2)
+            seeds=coords, radius=float(node_size), allow_overlap=True, memory_level=5,
+            memory='nilearn_cache', verbose=2, standardize=True)
         time_series = masker.fit_transform(func_file)
         for time_serie, label in zip(time_series.T, labels):
             plt.plot(time_serie, label=label)
@@ -202,7 +229,7 @@ def import_mat_func(input_file, ID, atlas_select, NETWORK, pynets_dir, node_size
     from sklearn.covariance import GraphLassoCV
     estimator = GraphLassoCV()
     est = estimator.fit(mx)
-    if NETWORK != '':
+    if NETWORK != None:
         est_path1 = dir_path + '/' + ID + '_' + NETWORK + '_est_cov.txt'
         est_path2 = dir_path + '/' + ID + '_' + NETWORK + '_est_sps_inv_cov.txt'
     else:
@@ -217,7 +244,7 @@ def cov_plt_func(mx, est_path1, ID, NETWORK):
     dir_path = os.path.dirname(os.path.realpath(est_path1))
     est_cov = genfromtxt(est_path1)
     rois_num=est_cov.shape[0]
-    if NETWORK != '':
+    if NETWORK != None:
         print("Creating Covariance plot of dimensions:\n" + str(rois_num) + ' x ' + str(rois_num))
         plt.figure(figsize=(rois_num, rois_num))
     else:
@@ -230,7 +257,7 @@ def cov_plt_func(mx, est_path1, ID, NETWORK):
     plt.title('Covariance')
     A=np.matrix(est_cov)
     G=nx.from_numpy_matrix(A)
-    if NETWORK != '':
+    if NETWORK != None:
         G = nx.write_graphml(G, dir_path + '/' + ID + '_' + NETWORK + '.graphml')
         out_path=dir_path + '/' + ID + '_' + NETWORK + '_adj_mat_cov.png'
     else:
@@ -245,7 +272,7 @@ def sps_inv_cov_plt_func(mx, est_path2, ID, NETWORK):
     dir_path = os.path.dirname(os.path.realpath(est_path2))
     est_sps_inv_cov = genfromtxt(est_path2)
     rois_num=est_sps_inv_cov.shape[0]
-    if NETWORK != '':
+    if NETWORK != None:
         print("Creating Sparse Inverse Covariance plot of dimensions:\n" + str(rois_num) + ' x ' + str(rois_num))
         plt.figure(figsize=(rois_num, rois_num))
     else:
@@ -260,7 +287,7 @@ def sps_inv_cov_plt_func(mx, est_path2, ID, NETWORK):
     plt.title('Sparse inverse covariance')
     A=np.matrix(est_sps_inv_cov)
     G=nx.from_numpy_matrix(A)
-    if NETWORK != '':
+    if NETWORK != None:
         G = nx.write_graphml(G, dir_path + '/' + ID + '_' + NETWORK + '.graphml')
         out_path=dir_path + '/' + ID + '_' + NETWORK + '_adj_mat_sps_inv_cov.png'
     else:
@@ -277,7 +304,8 @@ def extractnetstats(est_path, ID, NETWORK, thr, out_file=None):
 
     ##Threshold, binarize, and normalize matrix
     in_mat_thr = bct.threshold_proportional(in_mat, float(thr))
-    in_mat_wei = bct.weight_conversion(in_mat_thr, 'normalize')
+    in_mat_wei = in_mat_thr
+    #in_mat_wei = bct.weight_conversion(in_mat_thr, 'normalize')
     in_mat_bin = bct.weight_conversion(in_mat_thr, 'binarize')
     in_mat_len = bct.weight_conversion(in_mat_thr, 'lengths')
 
@@ -352,12 +380,12 @@ def extractnetstats(est_path, ID, NETWORK, thr, out_file=None):
 	avg_rich_club_coeff = float('nan')
 
     if 'inv' in est_path:
-        if NETWORK != '':
+        if NETWORK != None:
             out_path = dir_path + '/' + ID + '_' + NETWORK + '_net_global_scalars_inv_sps_cov.csv'
         else:
             out_path = dir_path + '/' + ID + '_net_global_scalars_inv_sps_cov.csv'
     else:
-        if NETWORK != '':
+        if NETWORK != None:
             out_path = dir_path + '/' + ID + '_' + NETWORK + '_net_global_scalars_cov.csv'
         else:
             out_path = dir_path + '/' + ID + '_net_global_scalars_cov.csv'
@@ -367,8 +395,8 @@ def extractnetstats(est_path, ID, NETWORK, thr, out_file=None):
 class ExtractNetStatsInputSpec(BaseInterfaceInputSpec):
     est_path = File(exists=True, mandatory=True, desc="")
     sub_id = traits.Str(mandatory=True)
-    NETWORK = traits.Str(mandatory=True)
-    thr = traits.Str(mandatory=True)
+    NETWORK = traits.Any(mandatory=True)
+    thr = traits.Any(mandatory=True)
 
 class ExtractNetStatsOutputSpec(TraitedSpec):
     out_file = File()
@@ -410,7 +438,7 @@ def export_to_pandas(csv_loc, ID, NETWORK, out_file=None):
 class Export2PandasInputSpec(BaseInterfaceInputSpec):
     in_csv = File(exists=True, mandatory=True, desc="")
     sub_id = traits.Str(mandatory=True)
-    NETWORK = traits.Str(mandatory=True)
+    NETWORK = traits.Any(mandatory=True)
     out_file = File('output_export2pandas.csv', usedefault=True)
 
 class Export2PandasOutputSpec(TraitedSpec):
@@ -435,7 +463,7 @@ class Export2Pandas(BaseInterface):
 
 ##Create input/output nodes
 #1) Add variable to IdentityInterface if user-set
-inputnode = pe.Node(niu.IdentityInterface(fields=['in_file', 'ID', 'atlas_select', 'NETWORK', 'pynets_dir', 'thr', 'node_size']), name='inputnode')
+inputnode = pe.Node(niu.IdentityInterface(fields=['in_file', 'ID', 'atlas_select', 'NETWORK', 'pynets_dir', 'thr', 'node_size', 'mask']), name='inputnode')
 
 #2)Add variable to input nodes if user-set (e.g. inputnode.inputs.WHATEVER)
 inputnode.inputs.in_file = input_file
@@ -445,10 +473,11 @@ inputnode.inputs.NETWORK = NETWORK
 inputnode.inputs.pynets_dir = pynets_dir
 inputnode.inputs.thr = thr
 inputnode.inputs.node_size = node_size
+inputnode.inputs.mask = mask
 
 #3) Add variable to function nodes
 ##Create function nodes
-imp_est = pe.Node(niu.Function(input_names = ['input_file', 'ID', 'atlas_select', 'NETWORK', 'pynets_dir', 'node_size'], output_names = ['mx','est_path1', 'est_path2'], function=import_mat_func, imports=import_list), name = "imp_est")
+imp_est = pe.Node(niu.Function(input_names = ['input_file', 'ID', 'atlas_select', 'NETWORK', 'pynets_dir', 'node_size', 'mask'], output_names = ['mx','est_path1', 'est_path2'], function=import_mat_func, imports=import_list), name = "imp_est")
 cov_plt = pe.Node(niu.Function(input_names = ['mx', 'est_path1', 'ID', 'NETWORK'], output_names = ['est_path1'], function=cov_plt_func, imports=import_list), name = "cov_plt")
 sps_inv_cov_plt = pe.Node(niu.Function(input_names=['mx', 'est_path2', 'ID', 'NETWORK'], output_names = ['est_path2'], function=sps_inv_cov_plt_func, imports=import_list), name = "sps_inv_cov_plt")
 net_glob_scalars_cov = pe.Node(ExtractNetStats(), name = "ExtractNetStats1")
@@ -472,7 +501,8 @@ wf.connect([
                           ('atlas_select', 'atlas_select'),
                           ('NETWORK', 'NETWORK'),
 			  ('pynets_dir', 'pynets_dir'),
-			  ('node_size', 'node_size')]),
+			  ('node_size', 'node_size'),
+              ('mask', 'mask')]),
     (inputnode, cov_plt, [('ID', 'ID'),
                           ('NETWORK', 'NETWORK')]),
     (imp_est, cov_plt, [('mx', 'mx'),