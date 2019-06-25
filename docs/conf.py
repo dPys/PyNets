d7ace1ae1969d702075e269df460dd114791425f
dPys <dpisner@utexas.edu>
2019-06-25 00:33:55 -0500

    final

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ author = 'Derek Pisner'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '\'0.7\''
+release = '0.7'
 
 
 # -- General configuration ---------------------------------------------------
@@ -46,8 +46,6 @@ extensions = [
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
-    'readthedocs_ext.readthedocs',
-    'recommonmark'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -72,7 +70,7 @@ language = None
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = None
--- docs/index.rst
+++ docs/index.rst
@@ -1,5 +1,5 @@
 .. PyNets documentation master file, created by
-   sphinx-quickstart on Tue Jun 25 00:21:07 2019.
+   sphinx-quickstart on Tue Jun 25 00:33:28 2019.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
