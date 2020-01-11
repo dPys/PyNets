927a00f7c551314889d8d0b3f0704176a61518ae
dPys <dpisner@utexas.edu>
2020-01-11 01:06:05 -0600

    Add get_version.py

--- get_version.py
+++ get_version.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
 # emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
 # vi: set ft=python sts=4 ts=4 sw=4 et:
-# @Author: dPys
-# @Date:   2019-06-25 09:42:38
+"""A convenience tool for querying PyNets's version."""
 import sys
 import os.path as op
 
 
 def main():
+    """Print current PyNets version."""
     sys.path.insert(0, op.abspath('.'))
     from pynets.__about__ import __version__
     print(__version__)
