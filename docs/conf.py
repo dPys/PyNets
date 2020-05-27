9eaa5521c76cff0b38640a413fbcbfbf1dee8e12
dPys <dpisner@utexas.edu>
2020-05-26 19:27:53 -0500

    Update travis yml

--- docs/conf.py
+++ docs/conf.py
@@ -140,9 +140,9 @@ todo_include_todos = False
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'sphinx_rtd_theme'
+# html_theme = 'sphinx_rtd_theme'
 # html_theme = 'alabaster'
-# html_style = 'nature.css'
+html_style = '_static/nature.css'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
