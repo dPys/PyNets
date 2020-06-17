71b62ad96e553cfe99a17a9606f8900985fa437b
dPys <dpisner@utexas.edu>
2020-06-16 20:06:55 -0500

    [FIX] typo

--- pynets/cli/pynets_run.py
+++ pynets/cli/pynets_run.py
@@ -377,9 +377,9 @@ def get_parser():
                              'LimbicTempPole, ContA, ContB, ContC, DefaultA, DefaultB, DefaultC, TempPar. If listing '
                              'multiple RSNs, separate them by space. (e.g. -n \'Default\' \'Cont\' \'SalVentAttn\')\'.'
                              '\n')
-    parser.add_argument('-nonames',
-                        default=True,
-                        action='store_false',
+    parser.add_argument('-names',
+                        default=False,
+                        action='store_true',
                         help='Optionally use this flag if you wish to deactivate automated anatomical labeling of '
                              'nodes.\n')
     parser.add_argument('-vox',
@@ -666,7 +666,7 @@ def build_workflow(args, retval):
     plugin_type = args.plug
     if type(plugin_type) is list:
         plugin_type = plugin_type[0]
-    use_AAL_naming = args.nonames
+    use_AAL_naming = args.names
     verbose = args.v
     clust_mask = args.cm
     if clust_mask:
