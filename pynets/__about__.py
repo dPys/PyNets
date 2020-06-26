85cd0437c05ee3019c352311159ccc146b605cda
dPys <dpisner@utexas.edu>
2020-06-25 23:48:03 -0500

    [MAINT] Clean up deps in requirements.txt

--- pynets/__about__.py
+++ pynets/__about__.py
@@ -6,7 +6,7 @@ Base module variables
 #from ._version import get_versions
 #__version__ = get_versions()['version']
 #del get_versions
-__version__ = '0.9.99a'
+__version__ = '0.9.99b'
 
 __packagename__ = 'pynets'
 __copyright__ = 'Copyright 2020, Derek Pisner'
