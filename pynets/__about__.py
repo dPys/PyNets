bd634f7bb312e389d29a2e43c11009acd658f7c5
dPys <dpisner@utexas.edu>
2020-11-23 21:40:04 -0600

    [MAINT] Update version to 1.0.3

--- pynets/__about__.py
+++ pynets/__about__.py
@@ -6,7 +6,7 @@ Base module variables
 # from ._version import get_versions
 # __version__ = get_versions()['version']
 # del get_versions
-__version__ = "1.0.2"
+__version__ = "1.0.3"
 
 __packagename__ = "pynets"
 __copyright__ = "Copyright 2016, Derek Pisner"
