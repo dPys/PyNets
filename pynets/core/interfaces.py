c1fcc5ff2ac4843b516c0e8eadf5ab65eb830873
dPys <dpisner@utexas.edu>
2021-05-28 02:36:33 -0500

    Update dev build

--- pynets/core/interfaces.py
+++ pynets/core/interfaces.py
@@ -1815,7 +1815,6 @@ class RegisterAtlasDWI(SimpleInterface):
             t1_aligned_mni_tmp_path,
             t1w2dwi_bbr_xfm_tmp_path,
             t1w2dwi_xfm_tmp_path,
-            wm_gm_int_in_dwi_tmp_path,
             t1w_brain_tmp_path
         ]
         for j in reg_tmp:
@@ -3252,7 +3251,6 @@ class Tracking(SimpleInterface):
                 self._results["streams"] = None
                 self._results["dm_path"] = None
             tmp_files = [gtab_file_tmp_path,
-                         labels_im_file_tmp_path_wm_gm_int,
                          wm_in_dwi_tmp_path, gm_in_dwi_tmp_path,
                          vent_csf_in_dwi_tmp_path, t1w2dwi_tmp_path]
 
@@ -3296,9 +3294,9 @@ class Tracking(SimpleInterface):
                     os.system(f"rm -f {j} &")
 
         # Exercise caution when deleting copied recon_path
-        if recon_path is not None:
-            if os.path.isfile(recon_path):
-                os.remove(recon_path)
+        # if recon_path is not None:
+        #     if os.path.isfile(recon_path):
+        #         os.remove(recon_path)
 
         return runtime
 
